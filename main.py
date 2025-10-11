9dd6ac2ddbac954abe88e0664c2b3ea5e0371f53
Samuel Ortiz Bermudez <samuel.ortber@gmail.com>
2025-10-11 00:54:49 -0500

    descripcion API

--- main.py
+++ main.py
@@ -4,7 +4,28 @@ from database.config import get_db
 from fastapi.middleware.cors import CORSMiddleware
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Sistema de Gestión Médica",
+    description="API para la gestion hospitalaria.",
+    version="1.2.0",
+    openapi_tags=[
+        {
+            "name": "Patient",
+            "description": "Operaciones relacionadas con el manejo de pacientes.",
+        },
+        {"name": "Medic", "description": "Gestión de información de médicos."},
+        {"name": "Nurse", "description": "Gestion de información de enfermeras."},
+        {
+            "name": "Medical appointment",
+            "description": "Agendamiento y consulta de citas médicas.",
+        },
+        {
+            "name": "Diagnosis",
+            "description": "Registro y consulta de diagnósticos médicos.",
+        },
+        {"name": "Bill", "description": "Gestión de facturación y pagos."},
+    ],
+)
 
 cors = CORSMiddleware(
     app,
